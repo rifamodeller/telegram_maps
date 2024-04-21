675d598ffdaed33c75eb1235098bcc6c9ca3f808
rifamodeller <137773881+rifamodeller@users.noreply.github.com>
2024-04-21 22:18:08 +0500

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -1,111 +1,63 @@
-# Импортируем необходимые классы.
-from geo import load_map, geocode
-import logging
-import os
+from telebot import types
+import telebot
+from geo import geocode, load_map, capitals_europe
 import random
 
-import task
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Application, MessageHandler, filters, CommandHandler, CallbackContext
-from dotenv import load_dotenv
+bot = telebot.TeleBot(Token)
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv('TOKEN')
-
-# Запускаем логгирование
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
-)
-
-logger = logging.getLogger(__name__)
-timer_keyboard = [['30 сек', '60 сек', '5 мин'], ['Назад']]
+information = ''
+otvets = 0
+goagain = True
 
 
-def remove_job_if_exists(name, context):
-    """Удаляем задачу по имени.
-    Возвращаем True если задача была успешно удалена."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    yess = types.KeyboardButton('/Давай начнем')
+    no = types.KeyboardButton('/Нет, может позже')
+    markup.add(yess, no)
+    mess = f'''Привет, {message.from_user.first_name}.
+    Я хочу помочь тебе выучить страны и столицы мира'''
+    bot.send_message(message.chat.id, mess, reply_markup=markup)
 
 
-async def set_timer(t, update, context):
-    """Добавляем задачу в очередь"""
-    chat_id = update.effective_message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-
-    context.job_queue.run_once(task, int(t), chat_id=chat_id, name=str(chat_id), data=t)
-    text = f'Засек {t} с.!'
-    if job_removed:
-        text += 'Старая задача удалена'
-    await update.effective_message.reply_text(text, reply_markup=ReplyKeyboardMarkup(['/close']))
+@bot.message_handler()
+def text(message):
+    if message.text == '/Давай начнем':
+        while otvets != 10:
+            if goagain:
+                yes(message)
+    if message.text == '/Нет, может позже':
+        bot.send_message(message.chat.id, 'Прискорбно, тогда увидимся потом')
 
 
-async def task(context):
-    t = context.job.data
-    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! {t}c. прошли!')
+@bot.message_handler()
+def returnn(message):
+    global otvets
+    global goagain
+    if information.lower() == message.text.lower():
+        bot.send_message(message.chat.id, 'Ответ верен')
+        otvets += 1
+        goagain = True
+    else:
+        bot.send_message(message.chat.id, 'Ответ неверен')
+        otvets += 1
+        goagain = True
 
 
-async def on_start(update: Update, context: CallbackContext):
-    keyboard = ReplyKeyboardMarkup([['/dice', '/timer'], ['Назад']])
-    await update.message.reply_text(text='Чем могу помочь?', reply_markup=keyboard)
+@bot.message_handler()
+def yes(message):
+    global goagain
+    goagain = False
+    bot.send_message(message.chat.id, 'Как называется эта страна?')
+    i = random.randint(0, 9)
+    global information
+    information = capitals_europe[i]
+    print(information)
+    load_map(geocode(capitals_europe[i]))
+    with open('map.png', 'rb') as file:
+        bot.send_photo(message.chat.id, file)
+    bot.register_next_step_handler(message, returnn)
 
 
-async def on_dice(update: Update, context: CallbackContext):
-    keyboard = ReplyKeyboardMarkup([['6', '6x2', '20'], ['Назад']])
-    await update.message.reply_text(text='Хотите кинуть кубик?', reply_markup=keyboard)
-
-
-async def on_timer(update: Update, context: CallbackContext):
-    keyboard = ReplyKeyboardMarkup(timer_keyboard)
-    await update.message.reply_text(text='Сколько времени засечь?', reply_markup=keyboard)
-
-
-async def on_close(update, context):
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров!'
-    await update.message.reply_text(text, reply_markup=timer_keyboard)
-
-
-dices = ['⚀', '⚁ ', '⚂ ', '⚃ ', '⚄ ', '⚅']
-
-
-async def on_message(update: Update, context: CallbackContext):
-    text = update.message.text
-    if text == '6':
-        await update.message.reply_text(text=random.choice('⚀⚁⚂⚃⚄⚅'))
-    elif text == '6x2':
-        await update.message.reply_text(text=''.join(random.choices('⚀⚁⚂⚃⚄⚅', k=2)))
-    elif text == '20':
-        await update.message.reply_text(text=str(random.randint(1, 20)))
-    elif text == 'Скрыть':
-        await update.message.reply_text('Клавиатура скрыта', reply_markup=ReplyKeyboardMarkup())
-    elif text == 'Назад':
-        await on_start(update, context)
-    elif text == '30 сек':
-        await set_timer(30, update, context)
-    elif text == '60 сек':
-        await set_timer(60, update, context)
-    elif text == '5 мин':
-        await set_timer(300, update, context)
-
-
-def main():
-    application = Application.builder().token(BOT_TOKEN).build()
-
-    application.add_handler(CommandHandler('start', on_start))
-    application.add_handler(CommandHandler('dice', on_dice))
-    application.add_handler(CommandHandler('timer', on_timer))
-    application.add_handler(CommandHandler('close', on_close))
-    application.add_handler(MessageHandler(filters=filters.TEXT & ~filters.COMMAND, callback=on_message))
-
-    application.run_polling()
-
-
-if __name__ == '__main__':
-    main()
+bot.polling(none_stop=True)
